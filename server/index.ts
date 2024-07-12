292feada8129e6754c98a603c95502256e47773f
denriquem <demartinezgatell@gmail.com>
2024-07-12 16:25:40 +0200

    initial auth0 setup

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,41 @@
 import express from "express";
 import router from "./router";
+import { auth } from "express-openid-connect";
 import { PrismaClient } from "@prisma/client";
 import morgan from 'morgan'
+
+const { requiresAuth } = require('express-openid-connect');
+
+
+import dotenv from "dotenv";
 
 const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 3000;
+dotenv.config();
 
 app.use(morgan('dev'))
 app.use(express.json());
 app.use("/api", router);
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SESSION_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.IssuerBaseURL
+}
+
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
