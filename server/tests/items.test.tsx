b91c074882b97674b4303f2a2bcfbd9419fb8c1c
denriquem <demartinezgatell@gmail.com>
2024-10-10 18:15:59 +0200

    fix tests and update prisma

--- server/tests/items.test.tsx
+++ server/tests/items.test.tsx
@@ -16,39 +16,40 @@ afterAll(async () => {
 });
 
 describe("Retreive items", () => {
-    it.only("should get the available test items ", async () => {
+    it("should get the available test items ", async () => {
         const response = await supertest(app)
             .get("/api/items")
             .set("Authorization", `Bearer ${token}`);
         expect(response.status).toBe(200);
-        expect(response.body[0].title).toBe("Pair of Shoes");
+        expect(response.body[0].title).toBe("Wooden Chair");
     });
 
-    // it("should get a single item when the item id is passed in", async () => {
-    //     const response = await supertest(app)
-    //         .get("/api/item/2")
-    //         .set("Authorization", `Bearer ${token}`);
-    //     expect(response.status).toBe(200);
-    //     expect(response.body.title).toBe("Trumpet");
-    // });
+    it("should get a single item when the item id is passed in", async () => {
+        const response = await supertest(app)
+            .get("/api/item/2")
+            .set("Authorization", `Bearer ${token}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body.title).toBe("Coffee Table");
+    });
 });
 
-// describe("Create item", () => {
-//     it("should create an item", async () => {
-//         const newItem = {
-//             title: "new test item",
-//             description: "a brand new test item",
-//             userId: "1",
-//         };
-//         const response = await supertest(app)
-//             .post("/api/item")
-//             .set("Authorization", `Bearer ${token}`)
-//             .send(newItem);
-//         expect(response.status).toBe(200);
-//         expect(response.body.message).toEqual("item succesfully created");
-//         expect(response.body.newItem.id).toBeDefined();
+describe("Create item", () => {
+    it("should create an item", async () => {
+        const newItem = {
+            title: "new test item",
+            description: "a brand new test item",
+            userId: "1",
+        };
+        const response = await supertest(app)
+            .post("/api/item")
+            .set("Authorization", `Bearer ${token}`)
+            .send(newItem);
+        expect(response.status).toBe(200);
+        expect(response.body.message).toEqual("item succesfully created");
+        expect(response.body.newItem.id).toBeDefined();
 
-//         const newItemId = response.body.newItem.id;
-//         await prisma.item.delete({ where: { id: newItemId } });
-//     });
-// });
+        const newItemId = response.body.newItem.id;
+        await prisma.item.delete({ where: { id: newItemId } });
+    });
+});
