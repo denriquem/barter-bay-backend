e63415b878187355718eaae90df557003192dc2d
denriquem <demartinezgatell@gmail.com>
2024-10-15 18:39:40 +0200

    remove reaction handler

--- server/handlers/reactions.ts
+++ server/handlers/reactions.ts
@@ -39,3 +39,19 @@ export const getReactionsByComment = async (req: Request, res: Response) => {
         res.status(500).json({ error: errorMessage });
     }
 };
+
+export const removeReaction = async (req: Request, res: Response) => {
+    const reactionId = req.params.reactionId;
+    try {
+        await prisma.reaction.delete({
+            where: {
+                id: reactionId,
+            },
+        });
+        res.status(200).json({ message: `reaction ${reactionId} removed` });
+    } catch (error) {
+        const errorMessage =
+            error instanceof Error ? error.message : "Unknown error";
+        res.status(500).json({ error: errorMessage });
+    }
+};
