9b14db38937fe5d4d1e3658c966aa4f895e23ff6
denriquem <demartinezgatell@gmail.com>
2024-10-23 15:57:34 +0200

    server tweaks

--- server/apiServer.ts
+++ server/apiServer.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
-import { auth } from "express-oauth2-jwt-bearer";
+import { auth as jwtAuth } from "express-oauth2-jwt-bearer";
+import { auth, requiresAuth } from "express-openid-connect";
 import { PrismaClient } from "@prisma/client";
 import router from "./router";
 
@@ -11,15 +12,33 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
-const jwtCheck = auth({
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SESSION_SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.IssuerBaseURL,
+};
+
+app.use(auth(config));
+
+const jwtCheck = jwtAuth({
     audience: "https://barter-bay-api/",
     issuerBaseURL: "https://dev-m0wut2s6duzmzyqq.eu.auth0.com/",
     tokenSigningAlg: "RS256",
 });
 
-app.use(jwtCheck);
+app.get("/", (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
 
-app.use("/api", jwtCheck, router);
+app.get("/profile", requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
+
+// app.use("/api", jwtCheck, router);
+app.use("/api", router);
 
 app.get("/authorized", function (req, res) {
     res.send("Secured Resource");
